b906c28d40bf7e1c6b94fd91babe5d7c27825ce6
Chris Morris <chris.morris3@gmail.com>
2021-08-25 16:52:25 -0600

    Create db-backup.sh

---
+++ db-backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# backup of mysql database and site files.
+
+# config
+HOME="/home/YOUR_USERNAME"
+SITEDIR="YOUR_SITE_DIRECTORY"
+DBHOST="YOUR_DB_HOSTNAME"
+DBUSER="YOUR_DB_USERNAME"
+DBPASS="YOUR_DB_PASSWORD"
+DBNAME="YOUR_DB_NAME"
+NOWDATE=$(date +"%y%m%d")
+NOWDAY=$(date +"%d")
+BACKUPDIR="backups"
+MYSQLDUMP="$(which mysqldump)"
+
+# backup file check, delete, or creation
+TARGETPATH=$HOME/$BACKUPDIR/$SITEDIR/$NOWDAY
+if [ -d $TARGETPATH ]
+then
+rm -r $TARGETPATH
+mkdir -p $TARGETPATH
+else
+mkdir -p $TARGETPATH
+fi
+
+# create a GZIP of the directory inside the target path
+tar -zcf $TARGETPATH/${SITEDIR}_$NOWDATE.tar.gz ./$SITEDIR
+
+# dump the data into a SQL file inside the target path
+$MYSQLDUMP -u $DBUSER -h $DBHOST -p$DBPASS $DBNAME | gzip > $TARGETPATH/${DBNAME}_$NOWDATE.sql.gz
+
+# print a message for the logfile / output email
+printf "$SITEDIR has been backed up" | tee -a $LOGFILE
